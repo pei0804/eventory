ae0fb4d42c30d424ae86ca2a362d418b7ebb6d09
tikasan <satak47cpc@gmail.com>
2016-09-19 18:25:21 +0900

    [ios][delete]余分なコメント削除

--- client/Eventory/Codes/Controller/BaseNavigationViewController.swift
+++ client/Eventory/Codes/Controller/BaseNavigationViewController.swift
@@ -18,16 +18,4 @@ class BaseNavigationViewController: UINavigationController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
