7e7bd6a3a938c381c53776bbc819a31e5cd0a4d6
tikasan <satak47cpc@gmail.com>
2016-09-24 21:32:22 +0900

    [golang][fix]mainパッケージが煩雑になっていたため、API周りをパッケージングした

---
+++ server/api/method.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"database/sql"
+
+	"github.com/tikasan/eventory/server/define"
+	"github.com/tikasan/eventory/server/model"
+)
+
+func Check(db *sql.DB) {
+	adminLog, err := os.OpenFile("./log/admin.log", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return
+	}
+	defer adminLog.Close()
+
+	now := time.Now()
+	logger := log.New(adminLog, "[start]", log.LstdFlags)
+	logger.Println(now)
+	adminLog.Sync()
+
+	receiver := Request()
+	for {
+		receive, ok := <-receiver
+		if ok {
+			model.Insert(db, receive)
+		} else {
+			break
+		}
+
+	}
+
+	end := time.Now()
+	logger = log.New(adminLog, "[end]", log.LstdFlags)
+	logger.Println(end)
+	adminLog.Sync()
+}
+
+func Request() <-chan []model.Event {
+	now := time.Now()
+	atdn := make([]model.Inserter, define.SERACH_SCOPE)
+	connpass := make([]model.Inserter, define.SERACH_SCOPE)
+	//doorKeeper := make([]model.Inserter, define.SERACH_SCOPE)
+	allInserter := make([]model.Inserter, 0)
+
+	for i := 0; i < define.SERACH_SCOPE; i++ {
+		ym := now.AddDate(0, i, 0).Format("200601")
+		atdn[i].Url = fmt.Sprintf("https://api.atnd.org/events/?count=100&format=jsonp&callback=&ym=%s", ym)
+		atdn[i].Api = define.ATDN
+
+		connpass[i].Url = fmt.Sprintf("https://connpass.com/api/v1/event/?count=100&ym=%s", ym)
+		connpass[i].Api = define.CONNPASS
+
+		//doorKeeper[i].Url = fmt.Sprintf("https://api.doorkeeper.jp/events?page=%d", i)
+		//doorKeeper[i].Api = define.DOORKEEPER
+	}
+
+	allInserter = append(allInserter, atdn...)
+	allInserter = append(allInserter, connpass...)
+	//allInserter = append(allInserter, doorKeeper...)
+	allEvents := make(chan []model.Event, len(allInserter))
+	var wg sync.WaitGroup
+
+	go func() {
+		for _, a := range allInserter {
+			wg.Add(1)
+			go func(a model.Inserter) {
+				cli := model.NewInserter(a.Url, a.Api)
+				events, err := cli.Get()
+				if err != nil {
+					fmt.Fprint(os.Stderr, err)
+				}
+				allEvents <- events
+				wg.Done()
+			}(a)
+		}
+		wg.Wait()
+		close(allEvents)
+	}()
+	return allEvents
+}
+
+func Response(w http.ResponseWriter, db *sql.DB) {
+	event, err := model.EventAllNew(db)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	enc := json.NewEncoder(w)
+
+	if err := enc.Encode(event); err != nil {
+		http.Error(w, "encoding failed", http.StatusInternalServerError)
+		return
+	}
+}
