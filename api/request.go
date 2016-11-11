31aef1ab8187b1f292f1c41b93bf8cdcb5684345
tikasan <satak47cpc@gmail.com>
2016-11-11 15:40:37 +0900

    hack: APIパッケージをテストしやすいようにコードを関数にまとめた。まだ改善の必要あり。

--- api/request.go
+++ api/request.go
@@ -22,14 +22,12 @@ func NewInserter(rawurl string, rawapi int, token string) *Inserter {
 }
 
 type Inserter struct {
-	Url      string
-	RespByte []byte
-	Api      int
-	Token    string
+	Url   string
+	Api   int
+	Token string
 }
 
-func (i *Inserter) Get() (events []model.Event, err error) {
-
+func (i *Inserter) sendQuery() (respByte []byte) {
 	req, err := http.NewRequest("GET", i.Url, nil)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
@@ -43,64 +41,77 @@ func (i *Inserter) Get() (events []model.Event, err error) {
 	resp, err := client.Do(req)
 	defer resp.Body.Close()
 
-	respByte, err := ioutil.ReadAll(resp.Body)
+	respByte, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 		return
 	}
+	return respByte
+}
 
-	if i.Api == define.ATDN {
-		var at model.At
-		err = json.Unmarshal(respByte, &at)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			return events, nil
-		}
-		e := new(model.Event)
-		events = make([]model.Event, len(at.Events))
-		for i, v := range at.Events {
-			formater.CopyStruct(v.Event, e)
-			events[i] = *e
-			events[i].ApiId = define.ATDN
-		}
-
-	} else if i.Api == define.CONNPASS {
-		var cp model.Cp
-		err := json.Unmarshal(respByte, &cp)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			return events, nil
-		}
-
-		e := new(model.Event)
-		events = make([]model.Event, len(cp.Events))
-		for i, v := range cp.Events {
-			formater.CopyStruct(v, e)
-			events[i] = *e
-			events[i].ApiId = define.CONNPASS
-		}
-
-	} else if i.Api == define.DOORKEEPER {
-
-		var dk []model.Dk
-		err := json.Unmarshal(respByte, &dk)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			return events, nil
-		}
-
-		e := new(model.Event)
-		events = make([]model.Event, len(dk))
-		for i, v := range dk {
-			formater.CopyStruct(v.Event, e)
-			events[i] = *e
-			events[i].ApiId = define.DOORKEEPER
-		}
-
-	} else {
-
-		return events, errors.New("未知のAPIがセットされています。")
-
+func atdnJsonParse(respByte []byte) (events []model.Event, err error) {
+	var at model.At
+	err = json.Unmarshal(respByte, &at)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		return events, nil
+	}
+	e := new(model.Event)
+	events = make([]model.Event, len(at.Events))
+	for i, v := range at.Events {
+		formater.CopyStruct(v.Event, e)
+		events[i] = *e
+		events[i].ApiId = define.ATDN
 	}
 	return events, nil
+}
+
+func connpassJsonParse(respByte []byte) (events []model.Event, err error) {
+	var cp model.Cp
+	err = json.Unmarshal(respByte, &cp)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		return events, nil
+	}
+
+	e := new(model.Event)
+	events = make([]model.Event, len(cp.Events))
+	for i, v := range cp.Events {
+		formater.CopyStruct(v, e)
+		events[i] = *e
+		events[i].ApiId = define.CONNPASS
+	}
+	return events, nil
+}
+
+func doorkeeperJsonParse(respByte []byte) (events []model.Event, err error) {
+	var dk []model.Dk
+	err = json.Unmarshal(respByte, &dk)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		return events, nil
+	}
+
+	e := new(model.Event)
+	events = make([]model.Event, len(dk))
+	for i, v := range dk {
+		formater.CopyStruct(v.Event, e)
+		events[i] = *e
+		events[i].ApiId = define.DOORKEEPER
+	}
+	return events, nil
+}
+
+func (i *Inserter) Get() (events []model.Event, err error) {
+
+	respByte := i.sendQuery()
+
+	if i.Api == define.ATDN {
+		return atdnJsonParse(respByte)
+	} else if i.Api == define.CONNPASS {
+		return connpassJsonParse(respByte)
+	} else if i.Api == define.DOORKEEPER {
+		return doorkeeperJsonParse(respByte)
+	}
+	return events, errors.New("未知のAPIがセットされています。")
 }
