da9c33af6e1c93b42960b44749373f2c1dfab1eb
tikasan <satak47cpc@gmail.com>
2017-01-02 20:49:18 +0900

    [golang] add: データをハッシュで管理するので、そのためのカラムと削除検知のためのカラムも追加した

--- migrations/1_init.sql
+++ migrations/1_init.sql
@@ -1,7 +1,7 @@
 -- +migrate Up
 DROP TABLE IF EXISTS `m_event`;
 CREATE TABLE `m_event` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '裏で管理する用のId',
+  `id` int(11) NOT NULL COMMENT '裏で管理する用のId',
   `event_id` varchar(10) NOT NULL COMMENT 'api_id-event_id',
   `api_id` int(1) NOT NULL COMMENT 'API ID - eventID\nATDN 0\nCONNPASS 1\nDOORKIEEPER 2',
   `title` varchar(30) NOT NULL,
@@ -14,11 +14,11 @@ CREATE TABLE `m_event` (
   `place` text NOT NULL,
   `start_at` datetime NOT NULL,
   `end_at` datetime NOT NULL,
+  `data_hash` char(64) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `eventId` (`event_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2351 DEFAULT CHARSET=utf8;
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- +migrate Down
 DROP TABLE m_event;
