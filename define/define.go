a784cf392b46b3f39441d2fc8eb668adba6d40f5
tikasan <satak47cpc@gmail.com>
2017-01-19 13:41:40 +0900

    [golang] add: 設定ファイル用の定数

--- define/define.go
+++ define/define.go
@@ -1,10 +1,10 @@
 package define
 
+// API
 const ATDN_URL = "https://api.atnd.org/events/?count=100&format=jsonp&callback="
 const CONNPASS_URL = "https://connpass.com/api/v1/event/?count=100"
 const DOORKEEPER_URL = "https://api.doorkeeper.jp/events"
-
-const UPDATE_INFO = "updateInfo"
+const SERACH_SCOPE = 12
 
 const (
 	ATDN = iota
@@ -12,11 +12,13 @@ const (
 	DOORKEEPER
 )
 
+// setting
 const (
 	PRODUCTION = iota + 1
 	STAGING
 	TEST
 )
 
-// 検索範囲
-const SERACH_SCOPE = 12
+// data
+const UPDATE_INFO = "updateInfo"
+const DB_CONFIG = "eventory-production-setting/dbconfig.yml"
